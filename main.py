705d8cf8308295a1ef179c19f6fa368c9605a827
takvam <alexander8921@hotmail.no>
2026-09-09 23:41:27 +0200

    Add new main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-import requests
 from dotenv import load_dotenv
-import os
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent
 
 load_dotenv()
 
-# Test the setup
-print("✅ Packages imported successfully!")
 
-# Test environment variable
-api_key = os.environ.get("API_KEY", "not-set")
-print(f"✅ API_KEY: {api_key}")
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    source: list[str]
+    tools_used: list[str]
 
-# Test requests
-response = requests.get("https://api.github.com")
-print(f"✅ GitHub API status: {response.status_code}")
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_message(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+
+# llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+# response = llm.invoke("What is the meaning of life?")
+
+agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools=[])
