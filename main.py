fe8de4782d2d9e11dea66b5afe42081a5f629450
takvam <alexander8921@hotmail.no>
2026-09-21 23:38:10 +0200

    Add new email and make AI-agent focus on top 5 norwegian brands

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
 from tools import search_tool, wiki_tool, save_tool
 import logging
 
